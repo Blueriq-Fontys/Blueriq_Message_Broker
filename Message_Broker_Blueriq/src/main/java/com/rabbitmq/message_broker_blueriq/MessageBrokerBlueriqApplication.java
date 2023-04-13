2a4a4d14c342bb90541024090b1679f2945e8df7
Ferid Ilica <ferid_vvv03@live.nl>
2023-04-13 12:56:30 +0200

    commit rabbit mq project
    Created rabbit mq message broker project with a docker compose file which creates rabbit mq image.

---
+++ Message_Broker_Blueriq/src/main/java/com/rabbitmq/message_broker_blueriq/MessageBrokerBlueriqApplication.java
@@ -0,0 +1,14 @@
+package com.rabbitmq.message_broker_blueriq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBrokerBlueriqApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MessageBrokerBlueriqApplication.class, args);
+	}
+
+}
---
+++ Message_Broker_Blueriq/src/test/java/com/rabbitmq/message_broker_blueriq/MessageBrokerBlueriqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.message_broker_blueriq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBrokerBlueriqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
